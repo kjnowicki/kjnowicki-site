8b5ad0c380cc2cafcd228edcac96e53c4c6c2fe9
Krzysztof Nowicki <prokrzy@gmail.com>
2021-10-30 11:49:04 +0200

    global header root link

--- public_html/Components/GlobalHeader/globalHeader.js
+++ public_html/Components/GlobalHeader/globalHeader.js
@@ -8,6 +8,21 @@ const tag = "global-header";
 class GlobalHeader extends CustomElement {
     constructor() {
         super(path, name);
+        window.onload = () => {
+            this.home_link();
+        }
+    }
+
+    home_link = function () {
+        var checkEl = setInterval(() => {
+            let el = document.querySelector("#header h1");
+            if(el){
+                el.addEventListener('click', () => {
+                    window.location.href = router();
+                })
+                clearInterval(checkEl);
+            }
+        }, 100);
     }
 }
 
