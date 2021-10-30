732e8fe850f876cc0586c626879e25fa78287d46
Krzysztof Nowicki <prokrzy@gmail.com>
2021-10-30 11:48:28 +0200

    nav-card linking script

--- public_html/Components/NavigationCard/navigationCard.js
+++ public_html/Components/NavigationCard/navigationCard.js
@@ -8,6 +8,24 @@ const tag = "nav-card";
 class NavigationCard extends CustomElement {
     constructor() {
         super(path, name);
+        window.onload = () => {
+            this.linking();
+        }
+    }
+
+    linking = function () {
+        var checkEl = setInterval(() => {
+            let elements = document.querySelectorAll("nav-card a");
+            if(elements.length > 0) {
+                elements.forEach(element => {
+                    let url = router() + element.getAttribute('url');
+                    element.addEventListener('click',() => {
+                        window.location.href = url;
+                    });
+                });
+                clearInterval(checkEl);
+            }
+        }, 100)
     }
 }
 
