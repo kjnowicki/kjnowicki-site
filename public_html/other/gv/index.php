d95f761431d6c2c1387e9332583af3f288a448ab
Krzysztof Nowicki <prokrzy@gmail.com>
2021-10-30 15:55:54 +0200

    new gv page

---
+++ public_html/other/gv/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+	<head>	
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="../../style.css">
+		<script type="module" src="script.js"></script>
+		<title>GV</title>
+	</head>
+	<body style="background-color: black;">
+		<global-header></global-header>
+	</body>
+</html>
