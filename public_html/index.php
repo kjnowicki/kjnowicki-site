12ce234ed70df97137165c67d10aceedcef56d38
Krzysztof Nowicki <prokrzy@gmail.com>
2022-03-22 23:22:41 +0100

    meta tags change on index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,12 +1,16 @@
 <!doctype html>
 <html>
 	<head>	
+		<title>KJNowicki</title>
 		<meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Krzysztof Jan Nowicki. Automation Software Engineer online profile.">
+		<meta name="keywords" content="Krzysztof Nowicki, testing, automation, data, analysis, gdev, personal development, software development, python, java, js">
+		<meta name="author" content="Krzysztof Nowicki">
+		
 		<link rel="stylesheet" href="style.css">
 		<script type="module" src="script.js"></script>
-		<title>Nowicki Krzysztof</title>
 	</head>
 	<body style="background-color: black;">
 		<global-header></global-header>
